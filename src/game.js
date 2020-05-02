f1e96a6283e3eb3643447f58a33a0bedcedd07ed
Martin89PL <marcingladkowski@gmail.com>
2020-05-02 18:28:26 +0200

    Valid answers - simple resolve

--- src/game.js
+++ src/game.js
@@ -1,5 +1,9 @@
 export default class Game {
 
+    constructor() {
+        this.actualQuestion = null;
+    }
+
     setData(data) {
         this.data = data;
     }
@@ -10,22 +14,59 @@ export default class Game {
     }
 
     loadQuestion() {
-        const verb = this.data[this.randomInt()]
+        this.actualQuestion = this.data[this.randomInt(this.data.length)]
 
-        if (verb) {
-            let { infinitive, past, pastParticiple } = verb;
+        if (this.actualQuestion) {
+            let { infinitive, past, pastParticiple } = this.actualQuestion;
+
+            let toShow = this.randomInt(3);
+
+            const formElements = [
+                this.getInfinitiveElement(),
+                this.getPastElement(),
+                this.getPastParticipleElement()
+            ]
+
+            // hide some options
             this.getInfinitiveElement().value = infinitive;
             this.getPastElement().value = past;
             this.getPastParticipleElement().value = pastParticiple;
+
+            formElements.forEach((element, index) => {
+                if (index != toShow) {
+                    element.value = '';
+                }
+            })
         }
     }
 
-    randomInt() {
-        return 0 + Math.floor((this.data.length - 0) * Math.random());
+    randomInt(max) {
+        return 0 + Math.floor((max - 0) * Math.random());
     }
 
     addClickListenerOnPlay() {
         this.getPlayElement().addEventListener("click", () => {
+
+            // user answer
+            const answer = {
+                infinitive: this.getInfinitiveElement().value,
+                past: this.getPastElement().value,
+                pastParticiple: this.getPastParticipleElement().value
+            }
+
+            if (
+                this.actualQuestion.infinitive !== answer.infinitive
+                || this.actualQuestion.past !== answer.past
+                || this.actualQuestion.pastParticiple !== answer.pastParticiple
+            ) {
+                // show error box
+                throw 'Invalid answer!';        
+            }
+
+
+
+            // valid answers
+            // return result
             this.loadQuestion()
         })
     }
